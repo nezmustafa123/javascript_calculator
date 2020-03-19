6302cb601bf43bb7d53ea9f8e37366aa07f786c9
nezmustafa123 <nezmustafa123@gmail.com>
2020-03-19 18:31:45 +0000

    function changes

--- app.js
+++ app.js
@@ -4,21 +4,21 @@ var tempMem = "";
 const display = document.querySelector('#display');
 //parseInt(tempMem);
 
-function getHistory() {
-	//maipulate history
-	return document.getElementById('previous-operand').innerText;
-}
+//function getHistory() {
+//	//maipulate history
+//	return document.getElementById('previous-operand').innerText;
+//}
 //alert(getHistory());
 
-function printHistory(num) {
-	document.getElementById('previous-operand').innerText = num;
-}
+//function printHistory(num) {
+//	document.getElementById('previous-operand').innerText = num;
+//}
 
-function getOutput() {
-	return document.getElementById('display').innerText;
-
-}
-function printOutput(num) {
+//function getOutput() {
+//	return document.getElementById('display').innerText;
+//
+//}
+//function printOutput(num) {
 	//start with temp mem string 
 //	string plus number coerces into string
 	
@@ -26,15 +26,15 @@ function printOutput(num) {
 //	tempMem = tempMem + num
 	
 //console.log(tempMem); 
-	if (num) {
-		document.getElementById('display').innerText = num;
-	} else {
-		//		..convert to comma seperated value
-		document.getElementById('display').innerText = getFormattedNumber(tempMem);
-		 (getFormattedNumber(tempMem), 'getformattednumber');
-	}
-	
-}
+//	if (num) {
+//		document.getElementById('display').innerText = num;
+//	} else {
+//		//		..convert to comma seperated value
+//		document.getElementById('display').innerText = getFormattedNumber(tempMem);
+//		 (getFormattedNumber(tempMem), 'getformattednumber');
+//	}
+//	
+//}
 
 document.querySelectorAll('[data-value]').forEach(el => {
 	el.onclick = e => {
@@ -64,96 +64,123 @@ document.querySelector('#clear').onclick = () => {
 	display.innerText = 0;
 }
 
+document.querySelector('#decimal').onclick = () => {
+  if(!display
+   .innerText
+   .split(/[\/\+\*\-]/)
+   .slice(-1)[0] 
+   .includes('.')) display.textContent += '.'
+}
 
-
-function getFormattedNumber(num) {
+document.querySelectorAll('#divide,#multiply,#add,#subtract').forEach(operator => operator.onclick = e => {
+//  console.log(display.innerText)
+	const operator = e.target.dataset.value;
 	
-	var n = Number(num);
-//	var value = n.toLocaleString("en");
-//	console.log(n)
-	return n;
-
+  if([...'+*/-'].includes(display.innerText.slice(-1)) 
+	// && operator!=='-'
+	) { /// <- checks the last character in the display
+	  display.innerText = display.innerText.slice(0,-1) + operator // <- removes the last char before adding the new operator
+	  //display.innerText += e.target.dataset.value
+} else {
+	display.innerText += operator // <- just adds the new operator
 }
-
-function clearOutput() {
-	{
-		tempMem = 0;
-	}
-}
-
-function reverseNumberFormat(num) {
-	return Number(num.replace(/,/g, ''));
-}
-
-var operator = document.getElementsByClassName("operator disabled");
-for (var i = 0; i < operator.length; i++) {
-	operator[i].addEventListener('click', function () {
-		if (this.id === "clear") {
-		   clearOutput();
-//			printHistory(0);
-//			printOutput(0);
+});
 
 
-		} else if (this.id === "backspace") 
-		{ var output = reverseNumberFormat(getOutput()).toString();
-			if (output) { //if output has a value
-				output = output.substr(0, output.length - 1);
-				console.log(Number(printOutput(output)));
-			}
-		} else {
-			var output = getOutput();
-			var history = getHistory();
-			if (output === "" && history != "") {
-				if (isNaN(history[history.length - 1])) {
-//					history = history.substr(0, history.length - 1);
-				}
-			}
-			if (output != "" || history != "") {
-				output = output === "" ?
-					output : reverseNumberFormat(output);
-
-				history = history + output;
-				if (this.id === "=") {
-					var result = eval(history);
-					printOutput(result);
-					printHistory("");
 
 
-				} else {
-					history = this.id;
-//					console.log(history);
-					printHistory(history);
-					printOutput("");
 
-				
-				}
-			}
-		}
 
-//      console.log(history);
-	});
-}
 
-function toString(num) {
-	
-}
-function getTarget(btnVal) {
-	
-	if(btnVal === del){
-    tempMem = "";
-	}
-	else if(btnVal === clear) {
+
+//function getFormattedNumber(num) {
+//	
+//	var n = Number(num);
+////	var value = n.toLocaleString("en");
+////	console.log(n)
+//	return n;
+//
+//}
+//
+//function clearOutput() {
+//	{
 //		tempMem = 0;
-	}
-	else 
-	{
-	tempMem = tempMem + btnVal.toString();
-	}
-
-	printOutput(tempMem);
-	console.log(btnVal, tempMem);
-	
-}
+//	}
+//}
+//
+//function reverseNumberFormat(num) {
+//	return Number(num.replace(/,/g, ''));
+//}
+//
+//var operator = document.getElementsByClassName("operator disabled");
+//for (var i = 0; i < operator.length; i++) {
+//	operator[i].addEventListener('click', function () {
+//		if (this.id === "clear") {
+//		   clearOutput();
+////			printHistory(0);
+////			printOutput(0);
+//
+//
+//		} else if (this.id === "backspace") 
+//		{ var output = reverseNumberFormat(getOutput()).toString();
+//			if (output) { //if output has a value
+//				output = output.substr(0, output.length - 1);
+//				console.log(Number(printOutput(output)));
+//			}
+//		} else {
+//			var output = getOutput();
+//			var history = getHistory();
+//			if (output === "" && history != "") {
+//				if (isNaN(history[history.length - 1])) {
+////					history = history.substr(0, history.length - 1);
+//				}
+//			}
+//			if (output != "" || history != "") {
+//				output = output === "" ?
+//					output : reverseNumberFormat(output);
+//
+//				history = history + output;
+//				if (this.id === "=") {
+//					var result = eval(history);
+//					printOutput(result);
+//					printHistory("");
+//
+//
+//				} else {
+//					history = this.id;
+////					console.log(history);
+//					printHistory(history);
+//					printOutput("");
+//
+//				
+//				}
+//			}
+//		}
+//
+////      console.log(history);
+//	});
+//}
+//
+//function toString(num) {
+//	
+//}
+//function getTarget(btnVal) {
+//	
+//	if(btnVal === del){
+//    tempMem = "";
+//	}
+//	else if(btnVal === clear) {
+////		tempMem = 0;
+//	}
+//	else 
+//	{
+//	tempMem = tempMem + btnVal.toString();
+//	}
+//
+//	printOutput(tempMem);
+//	console.log(btnVal, tempMem);
+//	
+//}
 
 
 
